bdf2dac968995da1ec870c7d3d70b9d6d5946d82
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-24 16:38:56 +0530

    creating instance

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -10,14 +10,19 @@ DOMAIN_Name="devsecopstrainee.site"
 #for instance in ${INSTANCES[@]}
 for instance in $@ #dynamically at the time of running the script we have to pass the arguments to install
 do
-    
+    #creating instance through cli
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0e357cdf3695bf2f9 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=text}]" --query "Instances[0].InstanceId" \--output text)
     if [ $INSTANCES != "frontend" ]
     then
-        IP=aws ec2 descripe-instances --instance-ids $INSTANCE_ID --query "Instances[0].PrivateIpAddress" \--output text
-        echo "the instance was $INSTANCE_ID"
+        IP=$(aws ec2 descripe-instances --instance-ids $INSTANCE_ID --query "Instances[0].PrivateIpAddress" \--output text)
+        
     else
-        IP=aws ec2 descripe-instances --instance-ids $INSTANCE_ID --query "Instances[0].PublicIpAddress" \--output text
+        IP=$(aws ec2 descripe-instances --instance-ids $INSTANCE_ID --query "Instances[0].PublicIpAddress" \--output text)
     fi
-
+    echo "$INSTANCE_ID ip address is $IP"
 done
+
+
+#In the above logic we are creating instance at line 14 with diffrent names mentioned at line 5
+#if it is not front end as we mentioned condition at line 15, we will take its private ip and we will print at line 22
+#if its a frontend instance we will take its public ip and we will print.
