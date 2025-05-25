00f2886378dabc0de3f590c348c81151392f4b46
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-25 15:54:17 +0530

    roboshop

---
+++ frontend.sh
@@ -0,0 +1,53 @@
+#!bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOG_FOLDER="/var/log/shellpractice"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0e357cdf3695bf2f9"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z0748101MT6SJ25GGSYP"
+DOMAIN_Name="devsecopstrainee.site"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOG_FOLDER    #-p will check whether dir is there or not, if it not exits it will create the folder.
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR : Please process with root access $N"
+    exit 1
+else
+    echo -e "$G Root access granted please proceed $N"
+fi
+
+dnf module list nginx -y $>> $LOG_FILE
+
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y $>> $LOG_FILE
+VALIDATE $? "installing nginx"
+
+systemctl enable nginx
+systemctl start nginx
+VALIDATE $? "starting nginx"
+
+rm -rf /usr/share/nginx/html/*
+VALIDATE $? "removing default contend of nginx"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip $>> $LOG_FILE
+VALIDATE $? "Downloading frontendservice"
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+VALIDATE $? "Un-Zipping frontend service"
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "copying nginx conf"
+
+systemctl restart nginx
+VALIDATE $? "nginx starting"
