95008148ab57fe4092563eabe414d6eba7754d3a
Surya Prakash <suryasrinivas765@gmail.com>
2025-05-25 13:12:14 +0530

    roboshop

---
+++ mongodb.sh
@@ -0,0 +1,51 @@
+#!bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOG_FOLDER="/var/log/shellpractice"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0e357cdf3695bf2f9"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z0748101MT6SJ25GGSYP"
+DOMAIN_Name="devsecopstrainee.site"
+
+mkdir -p $LOG_FOLDER    #-p will check whether dir is there or not, if it not exits it will create the folder.
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR : Please process with root access $N"
+    exit 1
+else
+    echo -e "$G access granted please proceed $N"
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+Validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R ERROR : $2 was failed $N"
+        exit 1
+    else
+        echo -e "$G $2 was successful $N"
+    fi
+}
+
+cp monogo.repo /etc/yum.repos.d/mongo.repo | tee -a $LOG_FILE
+validate $? "copying repo data"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "installing mongodb"
+
+systemctl enable mongod
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "starting mongodb"
+
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
+VALIDATE $? "modifying file for allowing remote connections"
+
+systemctl restart mongod &>>$LOG_FILE
